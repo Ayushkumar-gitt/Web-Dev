059e46f34d9de3fa3f16e793075783d5abbb4c81
Ayush <raj309457@gmail.com>
2026-03-09 21:51:20 +0530

    JS

--- JS/Learning/script.js
+++ JS/Learning/script.js
@@ -1,37 +1,69 @@
 // console.log("hey")
-if (10 > 5 || 10 < 20 || 10 === 10) {
-    console.log("true")
+function sayHello(){
+    console.log("Hello js")
+}
+sayHello()
+
+function add(a,b){
+    console.log(a+b)
+    return a+b;
 }
 
-console.log(5 | 1)
-test()
-function test() { console.log("Hello world") }
+add(2,9)
 
-// let age = parseInt(prompt("Enter your age"))
+function guest(guest = "Ayush"){
+    console.log(`hi ${guest}`)
+}
+guest()
+let ans = 0;
+function unlin(...number){
+    // number.forEach(function(val){ // Both loops doing same work
+    //     ans += val
+    // })
+    for(let i = 0;i<number.length;i++){
+        ans += number[i]
+    }
+}
+// unlin(1,2,3,4,5)
+// console.log(ans)
 
-// if(age>=18){
-//     console.log("Adult")
-// }else{
-//     console.log("Minor")
+( function () {
+    console.log("Ayush")
+})();
+
+// if (10 > 5 || 10 < 20 || 10 === 10) {
+//     console.log("true")
 // }
 
+// console.log(5 | 1)
+// test()
+// function test() { console.log("Hello world") }
 
-let score = 30
-score > 35 ? console.log("Pass") : console.log("Fail")
+// // let age = parseInt(prompt("Enter your age"))
 
-let vall = parseInt(prompt("Enter number"))
+// // if(age>=18){
+// //     console.log("Adult")
+// // }else{
+// //     console.log("Minor")
+// // }
 
-switch (vall) {
-    case 1:
-        console.log("Sunday")
-        break;
-    case 2:
-        console.log("Monday")
-        break;
-    case 3:
-        console.log("Wednesday")
-        break;
-    default:
-        console.log("Wrong input")
-        break;
-}
+
+// let score = 30
+// score > 35 ? console.log("Pass") : console.log("Fail")
+
+// let vall = parseInt(prompt("Enter number"))
+
+// switch (vall) {
+//     case 1:
+//         console.log("Sunday")
+//         break;
+//     case 2:
+//         console.log("Monday")
+//         break;
+//     case 3:
+//         console.log("Wednesday")
+//         break;
+//     default:
+//         console.log("Wrong input")
+//         break;
+// }
