0fab414c6a12c70c5fed96a862c05b1a56f53769
Ayush <raj309457@gmail.com>
2026-03-17 13:06:20 +0530

    Reels project complete

--- JS/Dom/Reels/script.js
+++ JS/Dom/Reels/script.js
@@ -7,7 +7,8 @@ const reels = [
     video: "./1.mp4",
     image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e",
     shareCount: 34,
-    isFollowed: true
+    isFollowed: true,
+    isMuted: true
   },
   {
     username: "priya_verma",
@@ -17,7 +18,8 @@ const reels = [
     video: "./2.mp4",
     image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330",
     shareCount: 21,
-    isFollowed: false
+    isFollowed: false,
+    isMuted: true
   },
   {
     username: "rohan_mehta",
@@ -27,7 +29,8 @@ const reels = [
     video: "./3.mp4",
     image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d",
     shareCount: 48,
-    isFollowed: true
+    isFollowed: true,
+    isMuted: true
   },
   {
     username: "ananya_gupta",
@@ -37,7 +40,8 @@ const reels = [
     video: "./4.mp4",
     image: "https://images.unsplash.com/photo-1531123897727-8f129e1688ce",
     shareCount: 19,
-    isFollowed: false
+    isFollowed: false,
+    isMuted: true
   },
   {
     username: "karan_patel",
@@ -47,7 +51,8 @@ const reels = [
     video: "./5.mp4",
     image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d",
     shareCount: 27,
-    isFollowed: true
+    isFollowed: true,
+    isMuted: true
   },
   {
     username: "neha_kapoor",
@@ -57,7 +62,8 @@ const reels = [
     video: "",
     image: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1",
     shareCount: 40,
-    isFollowed: false
+    isFollowed: false,
+    isMuted: true
   },
   {
     username: "rahul_malhotra",
@@ -67,7 +73,8 @@ const reels = [
     video: "",
     image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2",
     shareCount: 15,
-    isFollowed: false
+    isFollowed: false,
+    isMuted: true
   },
   {
     username: "simran_kaur",
@@ -77,7 +84,8 @@ const reels = [
     video: "",
     image: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c",
     shareCount: 52,
-    isFollowed: true
+    isFollowed: true,
+    isMuted: true
   },
   {
     username: "vikram_singh",
@@ -87,7 +95,8 @@ const reels = [
     video: "",
     image: "https://images.unsplash.com/photo-1521119989659-a83eee488004",
     shareCount: 12,
-    isFollowed: false
+    isFollowed: false,
+    isMuted: true
   },
   {
     username: "isha_jain",
@@ -97,32 +106,39 @@ const reels = [
     video: "",
     image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
     shareCount: 61,
-    isFollowed: true
+    isFollowed: true,
+    isMuted: true
   }
 ];
 
-var htmlforreel = ""
-reels.forEach(function(val){
-    htmlforreel+= `
+var allreels = document.querySelector('.all-reels')
+
+function addData() {
+  var htmlforreel = ""
+  reels.forEach(function (val, idx) {
+    htmlforreel += `
             <div class="reels">
-                <video autoplay loop muted src="${val.video}"></video>
+                <video autoplay loop ${val.isMuted ? 'muted' : ''} src="${val.video}"></video>
+                <div class="mute" id=${idx}>
+                ${val.isMuted ? '<i class="ri-volume-mute-line"></i>' : '<i class="ri-volume-up-line"></i>'}
+                </div>
                 <div class="bottom">
                     <div class="right">
                         <img id="profilepic" src="${val.image}" alt="">
                         <h4>Saddie sink</h4>
                     </div>
-                    <button id="followbtn">${val.isFollowed?'Unfollow':'Follow'}</button>
+                    <button class="followbtn" id=${idx}>${val.isFollowed ? 'Unfollow' : 'Follow'}</button>
                 </div>
                 <div class="actionbtn">
-                    <div class="like">
-                        ${val.isLiked?'<i class="ri-heart-3-fill"></i>':'<i class="ri-heart-3-line"></i>'}
+                    <div class="like" id=${idx}>
+                        ${val.isLiked ? '<i class="ri-heart-3-fill"></i>' : '<i class="ri-heart-3-line"></i>'}
                         ${val.likeCount}
                     </div>
-                    <div class="comment">
+                    <div class="comment" id = ${idx}>
                         <i class="ri-chat-1-line"></i>
                         ${val.commentCount}
                     </div>
-                    <div class="share">
+                    <div class="share" id = ${idx}>
                         <i class="ri-share-forward-line"></i>
                         ${val.shareCount}
                     </div>
@@ -131,7 +147,59 @@ reels.forEach(function(val){
                     </div>
                 </div>
             `
-});
+  });
+  allreels.innerHTML = htmlforreel
+}
+addData()
+var like = document.querySelector('.like')
 
-var allreels = document.querySelector('.all-reels')
-allreels.innerHTML = htmlforreel
+allreels.addEventListener('click', function (val) {
+  // console.log(val.target)
+  if (val.target.className == 'like') {
+    if (!reels[val.target.id].isLiked) {
+      reels[val.target.id].likeCount++;
+      reels[val.target.id].isLiked = true;
+    } else {
+      reels[val.target.id].likeCount--;
+      reels[val.target.id].isLiked = false;
+    }
+  }
+
+  if (val.target.className == "followbtn") {
+
+    if (!reels[val.target.id].isFollowed) {
+      reels[val.target.id].isFollowed = true;
+    } else {
+      reels[val.target.id].isFollowed = false
+    }
+  }
+  if (val.target.className == 'comment') {
+    reels[val.target.id].commentCount++;
+  }
+
+  if (val.target.className == 'share') {
+    reels[val.target.id].shareCount++;
+  }
+
+  if (val.target.className == 'mute') {
+    var currentState = reels[val.target.id].isMuted;
+
+    reels.forEach(function (reel) {
+      reel.isMuted = true;
+    });
+
+    if (currentState) {
+      reels[val.target.id].isMuted = false;
+    }
+
+
+    // if (!reels[val.target.id].isMuted) {
+    //   reels[val.target.id].isMuted = true;
+    // }else{
+    //   reels[val.target.id].isMuted = false
+    // }
+  }
+  // console.log(reels[val.target.id].likeCount);
+  addData();
+
+})
