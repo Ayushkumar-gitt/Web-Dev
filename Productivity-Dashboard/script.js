08493e6164fe97531fb06ec79f435f97ff76062d
Ayush <raj309457@gmail.com>
2026-03-30 23:24:19 +0530

    Todo phase 1 done

--- Productivity-Dashboard/script.js
+++ Productivity-Dashboard/script.js
@@ -13,3 +13,16 @@ fullElemBack.forEach(function (back) {
         elementFullScreen[back.id].style.display = 'none'
     })
 })
+
+var taskinput = document.querySelector('input')
+var taskdetails = document.querySelector('textarea')
+var taskCheckbox = document.querySelector('#checkbox')
+var form = document.querySelector('.addtask form')
+
+form.addEventListener('submit',function(val){
+    val.preventDefault()
+    console.log(taskinput.value)
+    console.log(taskdetails.value)
+    console.log(taskCheckbox.checked);
+    
+})
