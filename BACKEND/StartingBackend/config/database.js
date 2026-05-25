878e148778bcf8686e05c14cbd651289415ecc6f
Ayush Kumar <120472968+Ayushkumar-gitt@users.noreply.github.com>
2026-05-25 17:37:16 +0530

    starting Mongo

--- BACKEND/StartingBackend/config/database.js
+++ BACKEND/StartingBackend/config/database.js
@@ -1,6 +1,6 @@
 const mongodb = require("mongoose")
 function connectDb() {
-    mongodb.connect("mongodb+srv://starsoul04_db_user:9yigOlydcQ2ADM3Q@cluster0.iap0hso.mongodb.net/notes")
+    mongodb.connect(process.env.MONGO_URI)
         .then(() => {
             console.log("Database connected");
         })
