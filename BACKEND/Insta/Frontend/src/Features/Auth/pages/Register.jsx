5baa5ac8a7293c77d4828ecde1c18bec7dce2b33
Ayush Kumar <120472968+Ayushkumar-gitt@users.noreply.github.com>
2026-06-05 20:52:44 +0530

    Register full stack done

--- BACKEND/Insta/Frontend/src/Features/Auth/pages/Register.jsx
+++ BACKEND/Insta/Frontend/src/Features/Auth/pages/Register.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 import axios from 'axios'
+import { useAuth } from '../hooks/useAuth'
 
 const Register = () => {
   const [username, setusername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const {handleRegister,loading} = useAuth()
+  const navigate = useNavigate()
+
+  if(loading){
+    return(
+      <h1>Loading...</h1>
+    )
+  }
+
+
   async function submitHandler(e) {
     e.preventDefault()
+    handleRegister(username,email,password).then((response)=>{
+      console.log(response);
+      navigate('/')
+    })
+
   }
   return (
     <main>
